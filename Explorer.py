63ce59d68f42f351a778ef81bd1d105cb1ebe5a5
ShenNoether <Shen.Noether@gmx.com>
2015-06-12 14:34:44 -0600

    seems to be working now

--- Explorer.py
+++ Explorer.py
@@ -1,15 +1,33 @@
+#!/usr/bin/python
 import lmdb
-env = lmdb.open('/home/andrew/.bitmonero/lmdb/', readonly=True)
-i = 0
+import sys
+env = lmdb.open('/home/andrew/.bitmonero/lmdb/' , max_dbs=2, readonly=True)
+#subdb = env.open_db('data.mdb')
+
+print"stat"
+print(env.stat())
+print""
+
+print"Keys"
 with env.begin() as txn:
-    for i in range(0, 200):
-        cursor = txn.cursor()
-        print("thing i = ", i)
-        for key, value in cursor:
-            print(key, value)
-        print (" ")
-        print(" ")
-        print ("next ")
-        cursor.next()
+    with txn.cursor() as curs:
+        for key, value in curs: # Iterate from first key >= '5'.
+            print((key, value))
 
+keyy = raw_input("key desired?")
+child_db = env.open_db(key = keyy)
 
+print("blah")
+i = 0
+top = 20
+if len(sys.argv) >= 2:
+    top = int(sys.argv[1])
+with env.begin() as txn:
+    with txn.cursor(child_db) as curs:
+        for a in curs:
+            print(a)
+        #if not curs.set_key(keyy):
+            #print("No values found for '"+keyy+"'")
+        #else:
+            #for a in enumerate(curs.iternext()):
+                #print("a:", a)
